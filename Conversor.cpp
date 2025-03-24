8ba79e00aebda6d1deb310cf29e67de86d4f1cc3
João Pedro <joao.pf.silva@unesp.br>
2025-03-24 19:41:20 -0300

    Revert "Junção C/K"

--- Conversor.cpp
+++ Conversor.cpp
@@ -5,23 +5,19 @@ int main () {
          int op;
          
          printf("Escolha sua conversao:\n\n");
-         printf("|1| Conversao C => F\n");
-         printf("|2| Conversao C => K\n\n");
+         printf("|1| Conversao ºC => ºF\n");
+         printf("|2| Conversao ºC => K\n\n");
          scanf ("%d", & op);
          
-         //hahaha
+         
          if (op == 1) {
-         printf("Digite a temperatura em C: ");
+         printf("Digite a temperatura em °C: ");
          scanf("%f", &C);
          float F = C * 1.8 + 32;
-         printf ("A temperatura %.1f em F  %.1f", C, F);
+         printf ("A temperatura %.1f°C em ºF é %.1f", C, F);
          } 
-         else if (op == 2)
-		 {
-         printf("Digite a temperatura em C: ");
-         scanf("%f", &C);
-         float k = C + 273.15;
-         printf ("A temperatura %.1f C em k %.2f", C, k);
+         else if (op == 2){
+             
          }
     
     }
